58982e362669f47c1ca814551eff38b638cbb98a
LuisCa <luismirandac92@gmail.com>
2024-11-30 00:41:09 -0600

    cambio de baseURL para producion

--- src/api/yugiohApi.js
+++ src/api/yugiohApi.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
 export const yugiohApi = axios.create({
-   baseURL: 'https://db.ygoprodeck.com/api/v7',
    //baseURL: 'http://localhost:5000',
+   //baseURL: 'https://db.ygoprodeck.com/api/v7',
+   baseURL: 'https://serverygoappcardguesser-production.up.railway.app',
+   // headers: {
+   //    'Content-Type': 'application/json',
+   //    'Access-Control-Allow-Origin': '*',
+   //    'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE',
+   //    'Access-Control-Allow-Headers': 'Content-Type'
+   // }
 });
